42bfa15863917a18d0c1ff4b72f1b8b5e16ba871
Njuci <augustinnjuci@gmail.com>
2023-05-29 00:48:24 +0200

    addfoeldName

--- publication/models.py
+++ publication/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class FichierCsv(models.Model):
+    nom=models.CharField(max_length=80,unique=True)
     fichier=models.FileField(upload_to="fichier")
     def get_fichier_nom(self):
         return self.fichier.name
