b2a09de6d46b956d16ce667b987c980bcd6b61a2
Njuci <augustinnjuci@gmail.com>
2023-08-06 23:45:55 +0200

    'publicationresultat'

--- publication/urls.py
+++ publication/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
 
-from .views import getfichier,get_fichierUploader,uploandingCsvfile,MyView,Email_envoie,Email_envoie3
+from .views import get_fichierUploader,uploandingCsvfile,MyView,Email_envoie,Email_envoie3
 
 urlpatterns = [
-    path('get/', getfichier),
+ 
     path('getFic/<str:id>',get_fichierUploader),
     path('uploadfile',uploandingCsvfile.as_view()),
-    path('email',Email_envoie.as_view(),name='email_envoie'),
-    path('emailk',Email_envoie3.as_view(),name='email_envoie3'),
+    path('email',Email_envoie3.as_view(),name='email_envoie'),
     path('up',MyView.as_view())
 ]
